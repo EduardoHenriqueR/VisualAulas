3437c3809d9e8675845e8497e5cb150411f3901f
aluno <Aluno@PR02-302-41.CSED.LAB>
2023-08-18 09:53:31 -0300

    PrimeiroComit

---
+++ Aula/Produto.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula;
+public class Produto
+{
+        public string? Nome { get; set; }
+        public double Preco { get; set; }
+
+        public string? Descricao {get; set;}
+
+        public DateTime Validacao {get; set;}
+
+    public override string ToString()
+    {
+        return $"Nome: {Nome} \nPreço: {Preco}  \n Descricao: {Descricao} \nValidade: {Validacao}";
+    }
+
+}
+
---
+++ Aula/Program.cs
@@ -0,0 +1,16 @@
+﻿using Aula;
+
+
+Produto produto = new Produto();
+produto.Nome = Console.ReadLine();
+produto.Preco = Convert.ToInt32(Console.ReadLine());
+produto.Descricao = Console.ReadLine();
+Console.WriteLine(produto.Nome);
+Console.WriteLine(produto);
+
+Produto produto1 = new Produto{
+    Nome = "Pão",
+    Preco = 15,
+    Validacao = DateTime.Now
+};
+Console.WriteLine(produto1);
